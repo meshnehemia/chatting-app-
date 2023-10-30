c286c58e083d28f470d6c1652a5901a337cf783f
meshnehemia <meshnehemia7@gmail.com>
2023-10-30 20:14:00 +0300

    updating new sign in to the database using php

--- php/signup.php
+++ php/signup.php
@@ -18,13 +18,22 @@
             if(in_array($image_ext,$extensions)){
                 $upload_image = '../images/'.$image_name;
                 move_uploaded_file($image['tmp_name'],$upload_image);
-                echo "correct image ", $image_name ;
+
+                $sql = `CREATE TABLE IF NOT EXISTS profiles(
+                            id INT AUTO_INCREMENT,
+                             fname VARCHAR(15),
+                             lname VARCHAR(15),
+                             password VARCHAR(200),
+                             email VARCHAR(50),
+                             image VARCHAR(60),
+                             PRIMARY KEY (id)
+                            );`;
+              $sql ="INSERT INTO profiles(fname,lname,password,email,image) VALUES ('{$fname}','{$lname}','{$password}','{$email}','{$image_name}');";
+                mysqli_query($conn,$sql);
             }else{
                 echo "incorrect image please try again ";
             }
-
         } else {
             echo "incorrect email";
         }
-
     }
